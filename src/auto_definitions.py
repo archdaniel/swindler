0093d0b5a3eb21bd446ffc8eecdc564fe40f94a9
Daniel Arcanjo <daniel.k.arcanjo@gmail.com>
2025-10-10 14:27:04 +0000

    verbose bullshit. creating with help of the computer is terribly inconvenient

--- src/auto_definitions.py
+++ src/auto_definitions.py
@@ -181,7 +181,7 @@ class ModelDataProfiler:
     """
     def __init__(self, data, categorical_features, numerical_features, target, categorical_features_order=None, verbose=True):
         self.verbose = verbose
-        self.data = self._load_data(data)
+        self.data = self._load_data(data, verbose=verbose)
         self.categorical_features = categorical_features
         self.numerical_features = numerical_features
         self.target = target
@@ -189,12 +189,12 @@ class ModelDataProfiler:
         self.model = None
         self.results = {}
 
-    def _load_data(self, data):
+    def _load_data(self, data, verbose=True):
         if isinstance(data, pd.DataFrame):
-            if self.verbose: print("Loading data for ModelDataProfiler from DataFrame...")
+            if verbose: print("Loading data for ModelDataProfiler from DataFrame...")
             return data.copy()
         elif isinstance(data, str):
-            if self.verbose: print(f"Loading data for ModelDataProfiler from file: {data}...")
+            if verbose: print(f"Loading data for ModelDataProfiler from file: {data}...")
             if data.endswith(".csv"):
                 return pd.read_csv(data)
             elif data.endswith(".parquet"):
